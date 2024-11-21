5de57599a4f80d6ae22e95037e36e6b8a02997a9
johnforfar <46335740+johnforfar@users.noreply.github.com>
2024-11-21 11:06:32 +1100

    Fix flake configuration and univalue inheritance

--- flake.nix
+++ flake.nix
@@ -1,4 +1,4 @@
-# Update flake.nix:
+# ./flake.nix:
 
 {
   description = "Multi-architecture DigiByte validator";
@@ -23,22 +23,29 @@
             allowUnsupportedSystem = true;
           };
         };
+
+        # Build univalue package
+        univalue = pkgs.callPackage ./pkgs/univalue {};
+
+        # Create digibyte package with dependencies
+        digibyte = pkgs.callPackage ./pkgs/digibyte {
+          inherit univalue;
+          inherit (pkgs.qt6)
+            qtbase
+            qttools;
+          inherit (pkgs)
+            wrapQtAppsHook;
+          withGui = !pkgs.stdenv.isDarwin;
+        };
+
+        digibyted = pkgs.callPackage ./pkgs/digibyte {
+          inherit univalue;
+          withGui = false;
+        };
       in
       {
-        packages = rec {
-          digibyte = pkgs.callPackage ./pkgs/digibyte {
-            inherit (pkgs) univalue;
-            inherit (pkgs.qt6)
-              qtbase
-              qttools;
-            inherit (pkgs)
-              wrapQtAppsHook;
-            withGui = !pkgs.stdenv.isDarwin;
-          };
-          digibyted = pkgs.callPackage ./pkgs/digibyte {
-            inherit (pkgs) univalue;
-            withGui = false;
-          };
+        packages = {
+          inherit univalue digibyte digibyted;
           default = digibyted;
         };
       }
--- pkgs/univalue/default.nix
+++ pkgs/univalue/default.nix
@@ -1,4 +1,4 @@
-# pkgs/univalue/default.nix
+# ./pkgs/univalue/default.nix:
 
 { lib
 , stdenv
@@ -34,57 +34,4 @@ stdenv.mkDerivation rec {
     license = licenses.mit;
     platforms = platforms.unix;
   };
-}
-
-# Update flake.nix:
-
-{
-  description = "Multi-architecture DigiByte validator";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
-
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachSystem [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ] (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
-            allowUnsupportedSystem = true;
-          };
-        };
-
-        univalue = pkgs.callPackage ./pkgs/univalue {};
-      in
-      {
-        packages = rec {
-          inherit univalue;
-          
-          digibyte = pkgs.callPackage ./pkgs/digibyte {
-            inherit univalue;
-            inherit (pkgs.qt6)
-              qtbase
-              qttools;
-            inherit (pkgs)
-              wrapQtAppsHook;
-            withGui = !pkgs.stdenv.isDarwin;
-          };
-          
-          digibyted = pkgs.callPackage ./pkgs/digibyte {
-            inherit univalue;
-            withGui = false;
-          };
-          
-          default = digibyted;
-        };
-      }
-    );
 }
